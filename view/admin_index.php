79f3175253dda0e8ded355ef4af80292ecc8d024
Howard Miller <howardsmiller@gmail.com>
2014-07-17 16:08:55 +0100

    new menu item

--- view/admin_index.php
+++ view/admin_index.php
@@ -1,3 +1,4 @@
 <p><a class="btn btn-info" href="<?php echo $this->Url('time/index') ?>">Times</a></p>
 <p><a class="btn btn-info" href="<?php echo $this->Url('date/index') ?>">Dates</a></p>
 <p><a class="btn btn-info" href="<?php echo $this->Url('limit/index') ?>">Limits</a></p>
+<p><a class="btn btn-info" href="<?php echo $this->Url('fares/index') ?>">Fares</a></p>
