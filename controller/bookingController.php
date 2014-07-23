335cb82bf650c29a475f6aa3b333c6761c8a10f9
Howard Miller <howardsmiller@gmail.com>
2014-07-23 15:10:21 +0100

    basic calendar now displays

--- controller/bookingController.php
+++ controller/bookingController.php
@@ -6,25 +6,26 @@ use core\coreController;
 use model\bookingModel;
 
 class bookingController extends coreController {
-    
+
     protected $bm;
 
     public function __construct() {
         parent::__construct();
         $this->bm = new bookingModel;
     }
-    
+
     public function startAction() {
         $this->View('header');
         $this->View('booking_start');
         $this->View('footer');
     }
-    
+
     public function dateAction() {
         $cal = $this->getLib('calendar');
-              
-        // 
-      
+
+        //
+        $cal->showMonth(11, 2014);
+
         $calendar = $cal->render(30, 28);
         $this->View('header');
         $this->View('booking_date', array(
@@ -32,5 +33,5 @@ class bookingController extends coreController {
         ));
         $this->View('footer');
     }
-    
+
 }
--- lib/calendar.php
+++ lib/calendar.php
@@ -3,7 +3,7 @@
 namespace lib;
 
 class calendar {
-    
+
     private function isoDays() {
         return array(
             1 => 'Monday',
@@ -15,8 +15,8 @@ class calendar {
             7 => 'Sunday',
         );
     }
-    
-    public function render($daysinmonth, $firstmonday) {
+
+    public function render($daysinmonth, $firstday) {
         $days = $this->isoDays();
         $html = '<table class="table">';
         $html .= '<thead>';
@@ -26,10 +26,42 @@ class calendar {
         }
         $html .= '</tr>';
         $html .= '</thead>';
-        
+
+        // dom starts as zero until we hit the first day
+        $dom = 0;
+        $html .= '<tbody>';
+        for ($row=1; $row<=5; $row++) {
+            $html .= '<tr>';
+            foreach ($days as $daynum => $day) {
+                if (!$dom and ($daynum == $firstday)) {
+                	$dom = 1;
+                }
+                if (!$dom or ($dom > $daysinmonth)) {
+                	$html .= '<td>&nbsp;</td>';
+                } else {
+                	$html .= '<td>'.$dom.'</td>';
+                }
+                $dom++;
+            }
+            $html .= '</tr>';
+        }
+        $html .= '</tbody>';
+
         $html .= '</table>';
-        
+
         return $html;
-    }    
+    }
+
+    public function showMonth($month, $year) {
+
+    	// work out what day the first is
+    	$firstday = mktime(0, 0, 0, $month, 1, $year);
+    	$day = date('N', $firstday);
+
+    	// how many days in that month
+    	$dim = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+
+    	return $this->render($dim, $firstday);
+    }
 }
 
