1c3763b598e56a11c76f84b8da919f11607e38bc
Howard Miller <howardsmiller@gmail.com>
2014-08-02 23:00:03 +0100

    fixed a couple of incorrectly formatted forms

--- view/fares_edit.php
+++ view/fares_edit.php
@@ -1,6 +1,6 @@
 <h2>Update fares</h2>
 
-<form role="form" method="post" action="<?php echo $this->Url('fares/index/') ?>">
+<form class="form-horizontal" role="form" method="post" action="<?php echo $this->Url('fares/index/') ?>">
     <?php if ($errors) {$this->formErrors($errors);} ?>
     <?php $form->text('adult', 'Adult fare (&pound;)', number_format($fares->adult / 100, 2)); ?>
     <?php $form->text('child', 'Child fare (&pound;)', number_format($fares->child / 100, 2)); ?>
--- view/login.php
+++ view/login.php
@@ -1,6 +1,6 @@
 <h2>Login</h2>
 
-<form role="form" method="post" action="<?php echo $this->Url('user/login') ?>">
+<form class="form-horizontal" role="form" method="post" action="<?php echo $this->Url('user/login') ?>">
     <?php if ($errors) {$this->formErrors($errors);} ?>
     <?php $form->text('username', 'Username', ''); ?>
     <?php $form->password('password', 'Password'); ?>
