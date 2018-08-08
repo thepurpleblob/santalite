722fe2d0c3737695cf2bd40b52aee1c5bc6e396c
Howard Miller <howardsmiller@gmail.com>
2018-08-08 16:04:32 +0100

    start page more-or-less there

---
+++ src/asset/js/santa.js
@@ -0,0 +1,19 @@
+/**
+ * SRPS Santa Booking
+ *
+ * Copyright 2018, Howard Miller (howardsmiller@gmail.com)
+ *
+ * Javascript bits and bobs.
+ */
+
+/**
+ * Random header image
+ */
+$(function() {
+
+    // Random number 1..3
+    var random = 1 + Math.floor(Math.random() * 3);
+    var css = 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), ';
+    var url = 'src/asset/image/header' + random + '.jpg';
+    $('#bannerimg').css('background-image', css + 'url("' + url + '")');
+});
